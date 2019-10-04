30de7d37dbba7d58d9c38e918971de7adf8db2a0
Gabriel Brunet <gabriel_brunet@orange.fr>
2019-10-04 10:47:05 +0200

    add backgroung image for gifView, button, and activity background

---
+++ app/src/androidTest/java/com/example/snaptarget/GameActivityTest.java
@@ -0,0 +1,28 @@
+package com.example.snaptarget;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GameActivityTest {
+
+    @Test
+    public void getTimer() {
+    }
+
+    @Test
+    public void setTagToButton() {
+    }
+
+    @Test
+    public void launchTimer() {
+    }
+
+    @Test
+    public void random_all() {
+    }
+
+    @Test
+    public void actionColorButton() {
+    }
+}
